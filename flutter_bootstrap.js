d803becc0a43e5539ec96bfffa044ee2987198c7
ShashankGB <gbsshashank@gmail.com>
2026-07-20 20:47:36 +0530

    Built web
    Branch: main
    Commit: 6954281bd6fd88d718a3fbd92f3ed061d2556dcd (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "574289023"
+    serviceWorkerVersion: "2532680962"
   }
 });
